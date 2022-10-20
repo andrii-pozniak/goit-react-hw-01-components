d78f7f2e0e75acc0b34f7fe21884db135c4d0880
andrii-pozniak <andriipozniak@gmail.com>
2022-10-20 11:50:05 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import { Profile } from "./Profile/profile";
-import user from "";
+import user from './user.json';
 export const App = () => {
   return (
     <div>
       <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
+      username={user}
+      tag={user}
+      location={user}
+      avatar={user}
+      stats={user}
       />
     </div>
+    
   );
 };
