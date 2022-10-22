fbfef91550267edde3018d0c87aa4b7ea49bb5b0
andrii-pozniak <andriipozniak@gmail.com>
2022-10-22 15:32:32 +0300

    change task3

--- src/components/statsCard/statsCard.jsx
+++ src/components/statsCard/statsCard.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
 
+
 export const StatCards = ({label, percentage}) => {
-    return <li className="item">
+    return <li className="item" style={{backgroundColor: getRandomHexColor()}}>
       <span className="label_card">{label}</span>
       <span className="percentage">{percentage}%</span>
     </li>
@@ -13,3 +14,7 @@ StatCards.PropType = {
     percentage: PropTypes.number.isRequired,
 
 }
+
+function getRandomHexColor() {
+       return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    };
