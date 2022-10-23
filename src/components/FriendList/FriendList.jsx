e9d315482383fba953ecff483293adb69c8ad1e6
andrii-pozniak <andriipozniak@gmail.com>
2022-10-23 12:24:57 +0300

    fix task3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,13 @@
 import PropTypes from 'prop-types'
-import css from "./FriendList.module.css";
+import { FriendListItem } from "components/FriendListItem/FriendListItem";
 
 export const FriendList = ({cardsFriends}) => {
 return <ul className="friend_list">
-{cardsFriends.map(({avatar,name, isOnline, id}) => 
-   
-    <li className={css.item_friends} key={id}>
- <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
- <img className={css.avatar} src={avatar} alt={name} width="48" />
- <p className={css.name}>{name}</p>
- 
-</li>,
-
-)}
+{cardsFriends.map(({avatar,name, isOnline, id}) =>  
+<FriendListItem key={id} 
+    avatar={avatar}
+     name={name} 
+     isOnline={isOnline}/> )}
 </ul>
 
 }
